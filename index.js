16e4b78d780388a284115feaf8b723de63a0d091
Harun Unutgan <94921080+harun-u@users.noreply.github.com>
2022-10-18 10:58:53 +0300

    change code

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
+let myClock = document.querySelector('#myClock');
 let myName = document.querySelector('#myName');
 
 myName.innerHTML = prompt('İsminizi Girin: ');
+let date = new Date();
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let dayOfWeek = date.getDay();
 
 setInterval(function saat() {
-  let myClock = document.querySelector('#myClock');
-
   let date = new Date();
   let seconds = date.getSeconds();
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let dayOfWeek = date.getDay();
+
+  switch (dayOfWeek) {
+    case 0:
+      return (dayOfWeek = 'Pazar');
+      break;
+    case 1:
+      return (dayOfWeek = 'Pazartesi');
+      break;
+    case 2:
+      return (dayOfWeek = 'Salı');
+      break;
+    case 3:
+      return (dayOfWeek = 'Çarşamba');
+      break;
+    case 4:
+      return (dayOfWeek = 'Perşembe');
+      break;
+    case 5:
+      return (dayOfWeek = 'Cuma');
+      break;
+    case 6:
+      return (dayOfWeek = 'Cumartesi');
+      break;
+    default:
+      'Yanlis Deger';
+  }
 
   myClock.innerHTML = hours + ':' + minutes + ':' + seconds + '  ' + dayOfWeek;
 }, 1000);
-
